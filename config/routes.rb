15d81ab97def87fe07e37c1bc1d12e1da0e7f02b
Abdias Viana <abdviana@gmail.com>
2021-01-19 17:52:42 -0300

    verificando usuario logado, acrescentando campo nome a users, criando dashboard como tela principal da aplicaćão

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  root to: "dashboard#index"
 end
